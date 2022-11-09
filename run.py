826522b41f00c1e4c1b622adb15fe3a841473574
rinalds98 <rinalds98@gmail.com>
2022-11-09 12:44:25 +0000

    Add comments to code

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
+"""
+This project is a simple expense tracker for personal use. It tracks the users
+expenses and income. The user can input their incoem and expenses in the
+terminal. Check how much they have spent or saved.
+"""
+
 import gspread
 from google.oauth2.service_account import Credentials
+
+# initialise APIs from google sheets to access data and
+# modify its contents
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +21,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("expense-tracker")
 
+
 sales = SHEET.worksheet("income")
-
 data = sales.get_all_values()
-
 print(data)
