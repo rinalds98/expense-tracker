413e4c9c126cd72f42c4c31ea1d198a94902a32f
rinalds98 <rinalds98@gmail.com>
2022-11-15 22:51:35 +0000

    Modified how data gets accessed from multiple points to a single function that distributes information

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ This project is a simple expense tracker for personal use. It tracks the users
 expenses and income. The user can input their incoem and expenses in the
 terminal. Check how much they have spent or saved.
 """
+
 import os
 from termcolor import colored, cprint
 from datetime import datetime, timedelta, date
@@ -28,16 +29,86 @@ DAY = TODAY.strftime("%m/%d/%Y")
 active_user = []
 
 
+def get_data():
+    """
+    Gets all the information from the spreadsheet and
+    parses it into useful information such as usernames
+    income data and expense data
+    """
+
+    income_data = SHEET.worksheet("income").get_all_values()
+    expense_data = SHEET.worksheet("expenses").get_all_values()
+    income_data.pop(0)
+    expense_data.pop(0)
+
+    data = income_data + expense_data
+    unique_users = []
+    for user in data:
+        if user[0] not in unique_users:
+            unique_users.append(user[0])
+
+    return income_data, expense_data, unique_users
+
+
+def get_username(unique_users):
+    """
+    Asks user for their username and checks the google spreadsheet
+    if it has been used previously. If it has it welcomes them. If not
+    it creates it. the user gets added to a 'active users' variable which
+    is used to get that specific users data or input new data under
+    their username.
+    """
+
+    print("Please Enter Your Username!")
+    user = input()
+    if user in unique_users:
+        active_user.append(user)
+        print(f"Welcome {user}!")
+    else:
+        print(f"would you like to use {user} as your username from now?")
+        print("press 'y' or 'n'")
+        answer = input()
+        if answer == "y":
+            active_user.append(user)
+            print("Done")
+        else:
+            print("thank you good bye")
+
+
+def get_money(income, expenses):
+    """
+    Takes the values from both income and expenses worksheets
+    (from google sheets) and calculates the total income/expenses.
+    Also calculates the amount saved.
+    """
+
+    result_one = 0
+    for i, j, k, l in income:
+        if i == active_user[0]:
+            result_one += int(l)
+
+    result_two = 0
+    for i, j, k, l in expenses:
+        if i == active_user[0]:
+            result_two += int(l)
+
+    print(f"Total Income: €{result_one}, Total Expenses: €{result_two}\n")
+    savings = result_one - result_two
+    print(f"You have currently saved: €{savings}\n")
+    input("Press Enter to go back to the main menu...\n")
+
+
 def input_income_expense():
     """
     Asks the user if they want to input income or expenses. Once they
     have selected the program asks the user what type and the amount.
     Data gets validated by the "validate_value" function.
     """
+
     income_options = ["salary", "other"]
     expense_options = ["entertainment", "bills", "food", "transportation"]
     print("1.Income or 2.Expense?")
-    valid = 3
+    valid = 2
     answer = validate_value(valid)
     if answer == 1:
         options = income_options
@@ -51,7 +122,7 @@ def input_income_expense():
         print("1.Entertainment\n2.Bills\n3.Food\n4.Transportation")
 
     data = [active_user[0], DAY]
-    valid = len(options) + 1
+    valid = len(options)
     choice = validate_value(valid)
     data.append(options[choice - 1])
 
@@ -63,52 +134,26 @@ def input_income_expense():
     return data, worksheet
 
 
-def get_money():
-    """
-    Takes the values from both income and expenses worksheets
-    (from google sheets) and calculates the total income/expenses.
-    Also calculates the amount saved.
-    """
-    income = SHEET.worksheet("income").get_all_values()
-    income.pop(0)
-    result_one = 0
-    for i, j, k, l in income:
-        if i == active_user[0]:
-            result_one += int(l)
-
-    expenses = SHEET.worksheet("expenses").get_all_values()
-    expenses.pop(0)
-    result_two = 0
-    for i, j, k, l in expenses:
-        if i == active_user[0]:
-            result_two += int(l)
-
-    print(f"Total Income: €{result_one}, Total Expenses: €{result_two}\n")
-    savings = result_one - result_two
-    print(f"You have currently saved: €{savings}\n")
-    input("Press Enter to go back to the main menu...\n")
-
-
-def specific_time_checker():
+def specific_time_checker(income, expenses):
     """
     Asks the user if they wish to check their
     income / expenses. The function allows the user
     to check specific amount of days ago how much they
     have spent or earned.
     """
+
     print("1.Income or 2.Expense?")
-    valid = 3
+    valid = 2
     answer = validate_value(valid)
     if answer == 1:
-        sheet = "income"
+        sheet = income
     if answer == 2:
-        sheet = "expenses"
+        sheet = expenses
 
     print("Chose how many days back you want to see ie.")
     print("'7' for 7 days or '30' for 30 days")
     days = int(input())
-    data = SHEET.worksheet(sheet).get_all_values()
-    data.pop(0)
+    data = sheet
 
     past = date.today() - timedelta(days)
     newlist = []
@@ -133,6 +178,7 @@ def update_worksheet(data, worksheet):
     Receives a list containing (date,type of expense and amount)
     that are to be inserted in the relevant worksheet.
     """
+
     print("Updating Worksheet...")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
@@ -145,6 +191,7 @@ def validate_value(valid):
     Raises ValueError if anything but an integers is added
     and if its within the range of options.
     """
+
     while True:
         try:
             value = int(input())
@@ -162,56 +209,12 @@ def validate_value(valid):
             else:
                 return value
 
-        if value < valid and value > 0:
+        if value <= valid and value > 0:
             return value
         else:
             text = "Sorry, not an available option."
             print(colored(text, "red", attrs=["reverse"]))
             continue
-
-
-def get_username():
-    """
-    Asks user for their username and checks the google spreadsheet
-    if it has been used previously. If it has it welcomes them. If not
-    it creates it. the user gets added to a 'active users' variable which
-    is used to get that specific users data or input new data under
-    their username.
-    """
-    usernames1 = SHEET.worksheet("expenses").col_values(1)  # expense list
-    usernames2 = SHEET.worksheet("income").col_values(1)
-
-    usernames1.pop(0)  # pop username text
-    usernames2.pop(0)  # pop username text
-    usernames = usernames1 + usernames2  # merge both lists together
-
-    users = []
-
-    def remove_dups(usernames):
-        """
-        strips usernames of any duplicates and
-        adds to new users list
-        """
-        for user in usernames:
-            if user not in users:
-                users.append(user)
-
-    remove_dups(usernames)
-
-    print("Please Enter Your Username!")
-    user = input()
-    if user in users:
-        active_user.append(user)
-        print(f"Welcome {user}!")
-    else:
-        print(f"would you like to use {user} as your username from now?")
-        print("press 'y' or 'n'")
-        answer = input()
-        if answer == "y":
-            active_user.append(user)
-            print("Done")
-        else:
-            print("thank you good bye")
 
 
 def main():
@@ -222,8 +225,10 @@ def main():
     It then calls the correct function that the user selected. When the
     user is done, they have an option to exit the program.
     """
-
-    get_username()
+    print("Welcome to your personal expense tracker!")
+    print("Please wait while we load everything up...")
+    income_data, expense_data, unique_users = get_data()
+    get_username(unique_users)
     print("What would you like to do today?")
 
     while True:
@@ -233,11 +238,11 @@ def main():
         print("Option 3 - Check specific days ago")
         print("Option 4 - Exit")
         print("Please input a value '1', '2', '3' or '4' to select an option:")
-        valid = 5
+        valid = 4
         option = validate_value(valid)
 
         if option == 1:
-            get_money()
+            get_money(income_data, expense_data)
             os.system("clear")
             continue
         elif option == 2:
@@ -246,7 +251,7 @@ def main():
             # os.system("clear")
             continue
         elif option == 3:
-            specific_time_checker()
+            specific_time_checker(income_data, expense_data)
             os.system("clear")
             continue
         elif option == 4:
@@ -255,6 +260,9 @@ def main():
 
 main()
 
+
+# usernames1 = income_data.col_values(1)
+# print(income_data)
 
 # text = colored("Hello, World!", "red", )
 # print(colored("Wrong Answer!", "red", attrs=["reverse"]))
